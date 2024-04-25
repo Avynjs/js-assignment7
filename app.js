480c8180037342d200f934c7218f1930976d0594
Avynjs <500197118@fvtc.edu>
2024-04-25 12:35:23 -0500

    copied over assignments 7 and 8 to the correct repo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.static('public'))
@@ -20,11 +20,30 @@ app.get('/', (_, response) => {
 
 
 // GET /api/todos
+app.get('/api/todos', (request, response) => {
+    response.json(todos);
+});
 
 // POST /api/todos
+app.post('/api/todos', (request, response) => {
+    const todo = {
+        id: todos.length + 1,
+        item: request.body.item,
+        complete: false
+    };
+    todos.push(todo);
+    response.json(todo);
+});
 
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (request, response) => {
+    const { id } = request.params;
+    const todo = todos.find(todo => todo.id === parseInt(id));
+    if (!todo) return response.status(404).send('The todo with the given ID was not found.');
 
+    todo.complete = !todo.complete;
+    response.json(todo);
+});
 
 
 const message = `Server running: http://localhost:${port}`
